4b95691f5cd42a2822618565bdb2895c71542676
Dane <slip_dane@mail.ru>
2022-11-25 15:36:28 -0800

    Add_First_Task

--- Seminar_1/Homework/HW1.py
+++ Seminar_1/Homework/HW1.py
@@ -1,9 +1,10 @@
-def InputNumbers(inputText):
-    is_OK = false
-    while not is_OK:
-        try:
-            number = int(input(f"{inputText}"))
-            it_OK = True
-        except ValueError:
-            print("Это не число!")
-        return number
+# Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+week_day = int(input("Введите число : "))
+
+if 6 <= week_day <= 7 :
+    print ("Это выходной!")
+elif 0 < week_day < 7 :
+    print ("Это будний день!")
+else:
+    print ("Число не соответстует дню недели!")
